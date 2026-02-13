0e1cb27464c2f5bb1046fffc84247413da41d808
sun1217shine <51977778+sun1217shine@users.noreply.github.com>
2026-02-13 17:01:58 +0900

    2026ver-0213
    2026ver-0213

--- js/script.js
+++ js/script.js
@@ -6,6 +6,22 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
+    const porfileTxt = gsap.utils.toArray('.profile .txt');
+    porfileTxt.forEach(text => {
+        gsap.to(text, {
+            backgroundSize: '100%',
+            ease: 'none',
+            scrollTrigger: {
+                trigger: text,
+                start: 'center 96%',
+                end: 'center 70%',
+                scrub: 2,
+            },
+        });
+    });
+
+
+
 
 
 
